4db5fb58b7eab45421ed4d0e1afcbcdeb75158e0
Shah Alam <shahalam141072@gmail.com>
2022-11-20 10:01:06 +0600

    insertion sort is done

---
+++ sorting/selection-sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+// printArray funtion:
+int printArray(int arr[], int size){
+    for(int i=0;i<size;i++){
+        cout << "[" << i << "]" << "=" << arr[i] << endl;
+    }
+}
+
+
+int insertionSort(int arr[], int size){
+    for(int i=0;i<size-1;i++){
+        int min = i;
+        for(int j=i+1;j<size;j++){
+            if(arr[j]>arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+
+
+int main(){
+    // array input:
+    int n;
+    cout << "Enter array input" << endl;
+    cin >> n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    
+    // bubble sort:
+    insertionSort(arr,n);
+   
+
+     printArray(arr, n);
+}

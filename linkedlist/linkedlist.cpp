7803c071f3536589b3afa5ea9687249682d18cb0
Shah Alam <shahalam141072@gmail.com>
2023-01-12 11:34:04 +0600

    pointer 2 is done

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -1,23 +1,20 @@
 #include<iostream>
 using namespace std;
 
-struct list
-{
-    /* data */
-    int info;
-    struct list *next;
-};
-
-
 int main(){
+    int num1=4;
+    int num2=6;
 
-struct list *p,*q,*t;
-    p = new list;
-    cout << "Enter first element: ";
-    cin >> p->info;
+    int *p1,*p2;
 
-    q = p->next;
+    p1 = &num1;
+    p2 = &num2;
 
-    
+    cout <<  num1 << endl;
+    cout << num2 <<  endl;
+
+    cout <<  *p1 << endl;
+    cout << *p2 <<  endl;
+
 
 }

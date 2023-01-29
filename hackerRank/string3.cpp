c185399909cb846161da174f98134a236f154322
Shah Alam <shahalam141072@gmail.com>
2023-01-29 22:32:38 +0600

    ddone

--- hackerRank/string3.cpp
+++ hackerRank/string3.cpp
@@ -8,12 +8,6 @@ vector<int> parseInts(string str) {
 }
 
 int main() {
-    string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
-    }
-    
-    return 0;
+
+
 }
